f9dd8f873fb9e50662629db413531ad7af85d8f0
Satoshi Iwao <stsiwo@gmail.com>
2021-10-05 19:01:08 -0700

    fixed error

--- api/app/models/user.rb
+++ api/app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
   belongs_to :user_type
   # when the owner is deleted, what should we do about the chat rooms that the owner owns? delete the room or delete the
   # owner priviledge to another member.
-  has_many :chat_rooms, foreign_key: 'owner_id', class_name: 'ChatRoom', dependent: 'destroy'
+  has_many :chat_rooms, foreign_key: 'owner_id', class_name: 'ChatRoom', dependent: :destroy
 end
